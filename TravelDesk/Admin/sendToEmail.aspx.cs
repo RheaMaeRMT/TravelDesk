28e0c53876df5b0c9fe743cce03a7540d99e2ee8
Rhea Mae Trinidad <trinidadarheamae28@gmail.com>
2024-06-03 16:00:18 +0800

    FILE UPLOADED TO GDRIVE
    TO DO:
    DELETE IN DRIVE
    ATTACH GDRIVE LINKS TO EMAIL

--- TravelDesk/Admin/sendToEmail.aspx.cs
+++ TravelDesk/Admin/sendToEmail.aspx.cs
@@ -95,109 +95,44 @@ Please do not hesitate to reach out to me if you have any questions or require f
         }
 
 
-        //private void DownloadPdf()
-        //{
-        //    // Retrieve PDF bytes from session
-        //    byte[] pdfBytes = (byte[])Session["pdfBytes"];
-
-        //    // Retrieve ID and name for filename
-        //    string ID = Session["clickedRequest"].ToString();
-        //    string name = Session["travellerName"].ToString();
-        //    string filename = name + "_" + ID + ".pdf";
-
-        //    // Set content type and header for file download
-        //    Response.ContentType = "application/pdf";
-        //    Response.AddHeader("Content-Disposition", "attachment; filename=" + filename);
-
-        //    // Write the PDF bytes to the response
-        //    Response.BinaryWrite(pdfBytes);
-
-        //    // End the response
-        //    Response.End();
-        //}
-
-        private void loadDetailsForEmail()
+        private async Task loadDetailsForEmail()
         {
-            
-
-
             if (Session["travellerName"] != null && Session["userEmail"] != null)
             {
-
                 // Retrieve the current value of the emailMessage TextBox
                 string message = emailMessage.Text;
-                string[] filePaths = GetUploadedFilePaths();
                 // Retrieve the default message from the session variable
                 string defaultMessage = Session["DefaultMessage"].ToString();
 
                 // Compare the current message with the default message to see if it has been edited
                 if (message != defaultMessage)
                 {
-
                     // If the message has been edited, use the edited message
-                    // Pass the edited message to the JavaScript function
-                    string receiverEmail = Session["userEmail"].ToString();
-                    string name = Session["travellerName"].ToString();
-
                     message = HttpUtility.JavaScriptStringEncode(message);
-
-
-                    string script = $"<script>sendEmail('{receiverEmail}', '{message}', '{name}', {JsonConvert.SerializeObject(filePaths)});</script>";
-                    ClientScript.RegisterStartupScript(this.GetType(), "SendEmailScript", script);
                 }
                 else
                 {
-                    // If the message has been edited, use the edited message
-                    // Pass the edited message to the JavaScript function
-                    string receiverEmail = Session["userEmail"].ToString();
-                    string name = Session["travellerName"].ToString();
-
+                    // Use the default message
                     message = HttpUtility.JavaScriptStringEncode(defaultMessage);
-
-
-                    string script = $"<script>sendEmail('{receiverEmail}', '{message}', '{name}', {JsonConvert.SerializeObject(filePaths)});</script>";
-                    ClientScript.RegisterStartupScript(this.GetType(), "SendEmailScript", script);
                 }
-            }
 
+                string receiverEmail = Session["userEmail"].ToString();
+                string name = Session["travellerName"].ToString();
+
+                // Upload files to Google Drive and retrieve the links
+                List<string> driveLinks = new List<string>();
+                foreach (var postedFile in attachments.PostedFiles)
+                {
+                    string driveLink = UploadFileToGoogleDrive(postedFile);
+                    driveLinks.Add(driveLink);
+                }
+
+                // Pass the email details including the uploaded file links to the JavaScript function
+                string script = $"<script>sendEmailWithDriveLinks('{receiverEmail}', '{message}', '{name}', {JsonConvert.SerializeObject(driveLinks)});</script>";
+                ClientScript.RegisterStartupScript(this.GetType(), "SendEmailScript", script);
+            }
         }
 
-        // Upload file to Google Drive
-        public void UploadFileToDrive(string filePath, string folderId)
-        {
-            // OAuth 2.0 scopes required for Google Drive API
-            string[] scopes = { DriveService.Scope.DriveFile };
-
-            // Load client secrets JSON file (downloaded from Google Cloud Console)
-            GoogleCredential credential;
-            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
-            {
-                credential = GoogleCredential.FromStream(stream)
-                    .CreateScoped(scopes);
-            }
-
-            // Create Drive API service
-            var service = new DriveService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = "Travel Desk",
-            });
-
-            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
-            {
-                Name = Path.GetFileName(filePath),
-                Parents = new List<string> { folderId } // Folder ID where you want to upload the file
-            };
-            FilesResource.CreateMediaUpload request;
-            using (var stream = new FileStream(filePath, FileMode.Open))
-            {
-                request = service.Files.Create(fileMetadata, stream, "application/pdf");
-                request.Upload();
-            }
-            var file = request.ResponseBody;
-            Console.WriteLine("File ID: " + file.Id);
-            Console.WriteLine("File URL: " + file.WebViewLink);
-        }
 
         // Method to retrieve dynamically generated file paths
         private string[] GetUploadedFilePaths()
@@ -233,6 +168,197 @@ Please do not hesitate to reach out to me if you have any questions or require f
 
             return filePaths.ToArray();
         }
+        protected void ListUploadedFiles(string folderPath)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                string[] fileEntries = Directory.GetFiles(folderPath, "*.pdf");
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<ul>");
+
+                foreach (string filePath in fileEntries)
+                {
+                    string fileName = Path.GetFileName(filePath);
+
+                    sb.AppendFormat("<li>{0} <button type='button' onclick=\"deleteFile('{1}')\">X</button></li>", fileName, filePath.Replace("\\", "\\\\"));
+
+                }
+                sb.Append("</ul>");
+                fileListPlaceholder.Controls.Add(new Literal { Text = sb.ToString() });
+            }
+        }
+        private string UploadFileToGoogleDrive(HttpPostedFile postedFile)
+        {
+            try
+            {
+                // Path to the service account JSON key file
+                var serviceAccountCredentialFilePath = @"C:\Users\HR-OJT\source\repos\InnodataTravelDesk\TravelDesk\App_Data\service-account.json";
+
+                // Authenticate with the service account
+                GoogleCredential credential;
+                using (var stream = new FileStream(serviceAccountCredentialFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    credential = GoogleCredential.FromStream(stream)
+                        .CreateScoped(DriveService.Scope.DriveFile);
+                }
+
+                // Create the Drive service
+                var service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "TravelDesk",
+                });
+
+                // ID of the parent folder to upload to
+                string parentFolderId = "1QAIZ0j7KeaUaB0zbT1Pm9Qb2ITGCh1ti"; // Replace with your parent folder ID
+
+                // Get the subfolder name from the session
+                string subfolderName = Session["travellerName"].ToString();
+
+                // Ensure the subfolder exists (create if necessary)
+                string subfolderId = CreateSubfolderIfNotExists(service, parentFolderId, subfolderName);
+
+                // Upload the file to the subfolder
+                var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = Path.GetFileName(postedFile.FileName),
+                    Parents = new List<string> { subfolderId } // Ensure the file is uploaded to the subfolder
+                };
+
+                FilesResource.CreateMediaUpload request;
+                using (var stream = postedFile.InputStream)
+                {
+                    request = service.Files.Create(fileMetadata, stream, postedFile.ContentType);
+                    request.Fields = "id, webContentLink"; // Request the ID and web content link of the uploaded file
+                    request.Upload();
+                }
+
+                var file = request.ResponseBody;
+
+                // Log file upload success
+                Console.WriteLine("File uploaded successfully to Google Drive. File ID: " + file.Id);
+
+                // Construct the Google Drive link
+                string driveLink = file.WebContentLink;
+                Console.WriteLine("Google Drive link: " + driveLink);
+
+                // Return the Google Drive link
+                return driveLink;
+            }
+            catch (Google.GoogleApiException gEx)
+            {
+                // Log detailed Google API error
+                Console.WriteLine("Google API Error uploading file to Google Drive: " + gEx.Message);
+                Console.WriteLine("Google API Error Details: " + gEx.Error);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                // Log detailed error
+                Console.WriteLine("Error uploading file to Google Drive: " + ex.Message);
+                Console.WriteLine("Stack Trace: " + ex.StackTrace);
+                return null;
+            }
+        }
+        private string CreateSubfolderIfNotExists(DriveService service, string parentFolderId, string subfolderName)
+        {
+            // Check if the subfolder already exists
+            var listRequest = service.Files.List();
+            listRequest.Q = $"mimeType = 'application/vnd.google-apps.folder' and trashed = false and name = '{subfolderName}' and '{parentFolderId}' in parents";
+            listRequest.Fields = "files(id, name)";
+            var existingFolders = listRequest.Execute().Files;
+
+            if (existingFolders.Count > 0)
+            {
+                // Subfolder already exists
+                return existingFolders[0].Id;
+            }
+
+            // Create the subfolder
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = subfolderName,
+                MimeType = "application/vnd.google-apps.folder",
+                Parents = new List<string> { parentFolderId }
+            };
+
+            var request = service.Files.Create(fileMetadata);
+            request.Fields = "id";
+            var file = request.Execute();
+            return file.Id;
+        }
+
+        //protected void uploadButton_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        if (attachments.HasFile)
+        //        {
+        //            if (Session["travellerName"] != null)
+        //            {
+        //                string empFname = Session["travellerName"].ToString();
+        //                string subFolder = "otherFiles";
+        //                string folderPath = Server.MapPath("~/PDFs/travelArrangements/" + empFname + "/" + subFolder);
+
+        //                if (!Directory.Exists(folderPath))
+        //                {
+        //                    Directory.CreateDirectory(folderPath);
+        //                }
+
+        //                HttpFileCollection attachmentsCollection = Request.Files;
+
+        //                for (int i = 0; i < attachmentsCollection.Count; i++)
+        //                {
+        //                    HttpPostedFile attachment = attachmentsCollection[i];
+
+        //                    if (attachment.ContentLength > 0)
+        //                    {
+        //                        string filename = Server.HtmlEncode(empFname + "_" + Path.GetFileName(attachment.FileName));
+        //                        string extension = Path.GetExtension(filename).ToLower();
+
+        //                        if (extension == ".pdf")
+        //                        {
+        //                            if (attachment.ContentLength < 4100000)
+        //                            {
+        //                                string savePath = Path.Combine(folderPath, filename);
+        //                                attachment.SaveAs(savePath);
+        //                                // Upload file to Google Drive
+        //                                UploadFileToGoogleDrive(attachment);
+        //                            }
+        //                            else
+        //                            {
+        //                                Response.Write("<script>alert('File " + filename + " was not uploaded because the file size is more than 4MB.')</script>");
+        //                            }
+        //                        }
+        //                        else
+        //                        {
+        //                            Response.Write("<script>alert('Invalid File Upload. Please upload a PDF file.')</script>");
+        //                        }
+        //                    }
+        //                }
+
+        //                ListUploadedFiles(folderPath);
+        //                Response.Write("<script>alert('File uploaded successfully.')</script>");
+        //            }
+        //            else
+        //            {
+        //                Response.Write("<script>alert('Session Expired!'); window.location.href = '../LoginPage.aspx'; </script>");
+        //            }
+        //        }
+        //        else
+        //        {
+        //            Response.Write("<script>alert('No files inserted. Please attach the PDF file you want to upload.')</script>");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Error uploading file: " + ex.Message);
+        //        Response.Write("<script>alert('An error occurred while uploading the file. Please try again.')</script>");
+        //        Response.Write("<pre style='background: white;'>" + ex.ToString() + "</pre>");
+        //    }
+        //}
+
         protected void uploadButton_Click(object sender, EventArgs e)
         {
             try
@@ -251,6 +377,7 @@ Please do not hesitate to reach out to me if you have any questions or require f
                         }
 
                         HttpFileCollection attachmentsCollection = Request.Files;
+                        List<string> driveLinks = new List<string>();
 
                         for (int i = 0; i < attachmentsCollection.Count; i++)
                         {
@@ -267,6 +394,23 @@ Please do not hesitate to reach out to me if you have any questions or require f
                                     {
                                         string savePath = Path.Combine(folderPath, filename);
                                         attachment.SaveAs(savePath);
+
+                                        try
+                                        {
+                                            string driveLink = UploadFileToGoogleDrive(attachment);
+                                            if (!string.IsNullOrEmpty(driveLink))
+                                            {
+                                                driveLinks.Add(driveLink);
+                                            }
+                                            else
+                                            {
+                                                Response.Write("<script>alert('Failed to upload " + filename + " to Google Drive.')</script>");
+                                            }
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            Response.Write("<script>alert('Error uploading " + filename + " to Google Drive: " + ex.Message + "')</script>");
+                                        }
                                     }
                                     else
                                     {
@@ -281,7 +425,19 @@ Please do not hesitate to reach out to me if you have any questions or require f
                         }
 
                         ListUploadedFiles(folderPath);
-                        Response.Write("<script>alert('File uploaded successfully.')</script>");
+
+                        // Print the Google Drive links to the console (for debugging)
+                        foreach (var link in driveLinks)
+                        {
+                            Console.WriteLine("Uploaded to Google Drive: " + link);
+                        }
+
+                        if (driveLinks.Count > 0)
+                        {
+                            Session["UploadedDriveLinks"] = driveLinks;
+                        }
+
+                        Response.Write("<script>alert('Files uploaded successfully.')</script>");
                     }
                     else
                     {
@@ -301,25 +457,15 @@ Please do not hesitate to reach out to me if you have any questions or require f
             }
         }
 
-        protected void ListUploadedFiles(string folderPath)
+
+
+        protected void sendEmail_Click(object sender, EventArgs e)
         {
-            if (Directory.Exists(folderPath))
-            {
-                string[] fileEntries = Directory.GetFiles(folderPath, "*.pdf");
+            loadDetailsForEmail();
 
-                StringBuilder sb = new StringBuilder();
-                sb.Append("<ul>");
-
-                foreach (string filePath in fileEntries)
-                {
-                    string fileName = Path.GetFileName(filePath);
-
-                    sb.AppendFormat("<li>{0} <button type='button' onclick=\"deleteFile('{1}')\">X</button></li>", fileName, filePath.Replace("\\", "\\\\"));
-
-                }
-            }
         }
 
+        //DELETE PDF FILE (1 BY 1)
         protected void DeleteFileButton_Click(object sender, EventArgs e)
         {
             string filePath = hiddenFilePath.Value;
@@ -355,11 +501,5 @@ Please do not hesitate to reach out to me if you have any questions or require f
             }
         }
 
-
-        protected void sendEmail_Click(object sender, EventArgs e)
-        {
-            loadDetailsForEmail();
-
-        }
     }
 }
